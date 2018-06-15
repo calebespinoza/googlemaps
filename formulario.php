6611c364c313303629ac3085e84df4c7d3eede56
caleb@windows10 <cespinoza.capillacbba@outlook.com>
2018-06-15 02:12:02 -0400

    Probando Hooks

--- formulario.php
+++ formulario.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8"/>
-        <title>Monitoreo</title>
+        <title>Monitoreo Rio Rocha</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
     </head>
     <body>
